9eb1350510ac8b5e6936936aa8050496b3d14986
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-23 20:17:50 -0500

    Import those functions and use them to display user input data in DOM

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // importing other stuff, utility functions for:
 // working with supabase:
-import { checkAuth, signOutUser } from './fetch-utils.js';
+import { checkAuth, signOutUser, addItem, getItems } from './fetch-utils.js';
+import { renderShoppingList } from './render-utils.js';
 // pure rendering (data --> DOM):
 
 /*  "boiler plate" auth code */
@@ -15,9 +16,47 @@ signOutLink.addEventListener('click', signOutUser);
 /* end "boiler plate auth code" */
 
 // grab needed DOM elements on page:
+const listDiv = document.getElementById('list-div');
+const addItemForm = document.querySelector('.add-item-form');
 
 // local state:
 
+addItemForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addItemForm);
+    const item = formData.get('item');
+    const quantity = formData.get('quantity');
+
+    const response = await addItem(item, quantity);
+
+    addItemForm.reset();
+
+    const error = response.error;
+
+    if (error) {
+        console.log(error.message);
+    } else {
+        displayList();
+    }
+});
+
 // display functions:
+async function displayList() {
+    const items = await getItems();
+    
+    listDiv.innerHTML = '';
+
+    for (let item of items) {
+        const renderedItems = renderShoppingList(item);
+        listDiv.append(renderedItems);
+    }
+}
+
+async function loadData() {
+    await displayList();
+}
+
+loadData();
 
 // events:
